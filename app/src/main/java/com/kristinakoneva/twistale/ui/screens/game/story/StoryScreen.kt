2e4c52011a9cf5b774ff75775330ef86ce0ef742
Kristina Koneva <kristina.koneva@infinum.com>
2025-04-07 01:18:38 +0200

    Update UI of story screen

--- app/src/main/java/com/kristinakoneva/twistale/ui/screens/game/story/StoryScreen.kt
+++ app/src/main/java/com/kristinakoneva/twistale/ui/screens/game/story/StoryScreen.kt
@@ -1,19 +1,32 @@
 package com.kristinakoneva.twistale.ui.screens.game.story
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kristinakoneva.twistale.ui.screens.game.story.composables.StoryPartItem
+import com.kristinakoneva.twistale.ui.theme.spacing_1
+import com.kristinakoneva.twistale.ui.theme.spacing_2
+import com.kristinakoneva.twistale.ui.theme.spacing_3
+import com.kristinakoneva.twistale.ui.theme.spacing_4
+import com.kristinakoneva.twistale.ui.theme.spacing_5
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -24,7 +37,6 @@ fun StoryScreen(
     onNavigateToGameRoom: () -> Unit,
     viewModel: StoryViewModel = hiltViewModel(),
 ) {
-
     LaunchedEffect(viewModel.navigation) {
         viewModel.navigation.collect { event ->
             when (event) {
@@ -32,22 +44,63 @@ fun StoryScreen(
             }
         }
     }
-    viewModel.state.collectAsStateWithLifecycle().value.let { state ->
-        Column(
-            modifier = Modifier
-                .systemBarsPadding()
-                .verticalScroll(rememberScrollState())
-        ) {
-            Button(onClick = viewModel::endGame, modifier = Modifier.fillMaxWidth()) {
-                Text(text = "End game")
-            }
-            state.stories.forEach { story ->
-                LazyRow {
-                    items(story.storyParts.size) { index ->
-                        StoryPartItem(
-                            item = story.storyParts[index],
-                        )
+    Scaffold { padding ->
+        viewModel.state.collectAsStateWithLifecycle().value.let { state ->
+            Column(
+                modifier = Modifier
+                    .padding(padding)
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Column(modifier = Modifier.padding(horizontal = spacing_3)) {
+                    Spacer(modifier = Modifier.height(spacing_3))
+                    Row {
+                        Spacer(modifier = Modifier.weight(1f))
+                        Button(onClick = viewModel::endGame) {
+                            Text(text = "End game")
+                        }
                     }
+                    Spacer(modifier = Modifier.height(spacing_4))
+                    Text(
+                        text = "Story Time 🎉",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.headlineLarge,
+                        color = MaterialTheme.colorScheme.primary,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Spacer(modifier = Modifier.height(spacing_1))
+                    Text(
+                        text = "Explore all the tales you helped create together! 💪",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.titleLarge,
+                    )
+                    Spacer(modifier = Modifier.height(spacing_5))
+                }
+                state.stories.forEachIndexed { index, story ->
+                    Text(
+                        text = "Tale #${index + 1}",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = spacing_3),
+                        textAlign = TextAlign.Start,
+                        style = MaterialTheme.typography.headlineSmall,
+                        color = MaterialTheme.colorScheme.primary,
+                        fontWeight = FontWeight.SemiBold,
+                    )
+                    Spacer(modifier = Modifier.height(spacing_2))
+                    LazyRow(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(spacing_2)
+                    ) {
+                        items(story.storyParts.size) { index ->
+                            StoryPartItem(
+                                item = story.storyParts[index],
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(spacing_3))
                 }
             }
         }
--- app/src/main/java/com/kristinakoneva/twistale/ui/screens/game/story/composables/StoryPartItem.kt
+++ app/src/main/java/com/kristinakoneva/twistale/ui/screens/game/story/composables/StoryPartItem.kt
@@ -1,20 +1,33 @@
 package com.kristinakoneva.twistale.ui.screens.game.story.composables
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.kristinakoneva.twistale.domain.game.models.RoundType
 import com.kristinakoneva.twistale.domain.game.models.StoryPart
+import com.kristinakoneva.twistale.ui.theme.spacing_1
+import com.kristinakoneva.twistale.ui.theme.spacing_2
 
 @Composable
 fun StoryPartItem(
@@ -24,18 +37,72 @@ fun StoryPartItem(
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .padding(8.dp),
-        verticalArrangement = Arrangement.Center,
+            .width(300.dp)
+            .padding(horizontal = spacing_2)
+            .clip(shape = MaterialTheme.shapes.small)
+            .background(
+                shape = MaterialTheme.shapes.small,
+                color = MaterialTheme.colorScheme.primaryContainer,
+            )
+            .border(
+                width = 2.dp,
+                shape = MaterialTheme.shapes.small,
+                color = MaterialTheme.colorScheme.primary,
+            )
+            .padding(spacing_2),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(text = item.player.name, textAlign = TextAlign.Center)
-        Spacer(modifier = modifier.height(8.dp))
-        when (item.roundType) {
-            RoundType.WRITING -> Text(text = item.input, textAlign = TextAlign.Center)
-            RoundType.DRAWING -> AsyncImage(
-                model = item.input,
-                contentDescription = null,
+        val actionText = when (item.roundType) {
+            RoundType.WRITING -> "wrote ✍️:"
+            RoundType.DRAWING -> "drew 🎨:"
+        }
+        Row(
+            modifier = Modifier.align(Alignment.Start),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = item.player.name,
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .clip(shape = MaterialTheme.shapes.small)
+                    .background(
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = MaterialTheme.shapes.small,
+                    )
+                    .padding(spacing_1),
+                color = Color.White,
+                style = MaterialTheme.typography.titleSmall.copy(fontSize = 18.sp),
             )
+            Spacer(modifier = Modifier.width(spacing_1))
+            Text(
+                text = actionText,
+                style = MaterialTheme.typography.titleSmall.copy(fontSize = 18.sp),
+            )
+        }
+        Spacer(modifier = modifier.height(spacing_2))
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            when (item.roundType) {
+                RoundType.WRITING -> Text(
+                    text = "\"${item.input}\"",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 24.sp,
+                    ),
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold,
+                )
+
+                RoundType.DRAWING -> AsyncImage(
+                    model = item.input,
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth,
+                )
+            }
         }
     }
 }
